2c9c8961da73e5fb8017962189a7b9a1787e7a3c
rinae <emaren84@gmail.com>
2016-08-28 16:54:31 +0900

    Get right search results with Mechanize gem

--- app/controllers/used_books_controller.rb
+++ app/controllers/used_books_controller.rb
@@ -2,7 +2,8 @@ class UsedBooksController < ApplicationController
   require "nokogiri"
   require "open-uri"
   require "uri"
-  include UsedBooksHelper
+  require "net/http"
+  require "mechanize"
 
   def index
     get_used_books
@@ -11,26 +12,35 @@ class UsedBooksController < ApplicationController
   private
 
     # 매장 리스트
-    STORE_LISTS = %w(Gangnam Sinchon Geondae Nowon Daehakro
-                     Suyu Sillim Yeonsinnae Jamsil Sincheon Jongno Hapjeong)
+    STORE_LISTS = %w(gangnam sinchon geondae nowon daehakro
+                     suyu sillim yeonsinnae jamsil sincheon jongno hapjeong)
+
+    # 매장 방문 URI
+    STORE_URI = "http://off.aladin.co.kr/usedstore/wstoremain.aspx?offcode="
+
     # 검색 기본 URI
     SEARCH_URI = "http://off.aladin.co.kr/usedstore/wsearchresult.aspx?SearchWord="
 
-    # Unknown, but required
-    X_Y = URI.encode("&x=0&y=0".encode('euc-kr', 'utf-8'))
-
     def get_used_books
       # 찾고자 하는 책
-      target_book = URI.encode(params[:query].encode('euc-kr', 'utf-8'))
-      puts target_book
+      target_book = params[:query].encode('euc-kr', 'utf-8') # EUC-KR 인코딩을 한 뒤에 쿼리
       # 매장별 결과를 담을 해시
       result = {}
 
       STORE_LISTS.each do |store|
-        uri = SEARCH_URI + target_book + X_Y + URI.encode("?Offcode=#{store}".encode('euc-kr', 'utf-8'))
-        page = Nokogiri::HTML(open(uri, 'r:binary').read.encode('utf-8', 'euc-kr'))
-        titles = []
+        # 각 매장별 페이지 주소
+        to_store = URI.parse(STORE_URI + store)
+        # Mechanize 객체 생성
+        agent = Mechanize.new
 
+        # 각 매장별 인덱스 페이지 진입 후, 폼에 책 제목을 입력한 뒤에 제출, 그리고 그 결과를 받아온다.
+        store_index = agent.get(to_store)
+        search_form = store_index.form('QuickSearch')
+        search_form['SearchWord'] = target_book
+        page = agent.submit(search_form)
+        page.encoding = 'EUC-KR' # 결과 리턴받는데 반드시 인코딩을 지정해주어야 함. 안그러면 한글 깨짐
+
+        titles = []
         search_results = page.css('div.ss_book_box td a.bo_l b')
         search_results.each do |book|
           titles << book.text
