0c8f0483c09608ba6d646e46381bde9374fb75c0
rinae <emaren84@gmail.com>
2016-08-30 21:05:14 +0900

    Implement parsing data

--- app/controllers/used_books_controller.rb
+++ app/controllers/used_books_controller.rb
@@ -3,14 +3,14 @@ class UsedBooksController < ApplicationController
   require "mechanize"
 
   def index
-    get_used_books
   end
 
   private
 
     # 매장 리스트
-    STORE_LISTS = %w(gangnam sinchon geondae nowon daehakro
-                     suyu sillim yeonsinnae jamsil sincheon jongno hapjeong)
+    STORE_LISTS = { 'gangnam' => '강남점', 'sinchon' => '신촌점', 'geondae' => '건대점',  'nowon' => '노원점', 'daehakro' => '대학로점',
+                    'suyu' => '수유점', 'sillim' => '신림점', 'yeonsinnae' => '연신내점', 'jamsil' => '잠실롯데월드타워점',
+                    'sincheon' => '잠실신천점', 'jongno' => '종로점', 'hapjeong' => '합정점' }
 
     # 매장 방문 URI
     STORE_URI = "http://off.aladin.co.kr/usedstore/wstoremain.aspx?offcode="
@@ -24,7 +24,7 @@ class UsedBooksController < ApplicationController
       # 매장별 결과를 담을 해시
       result = {}
 
-      STORE_LISTS.each do |store|
+      STORE_LISTS.keys.each do |store|
         # 각 매장별 페이지 주소
         to_store = URI.parse(STORE_URI + store)
         # Mechanize 객체 생성
@@ -37,12 +37,33 @@ class UsedBooksController < ApplicationController
         page = agent.submit(search_form)
         page.encoding = 'EUC-KR' # 결과 리턴받는데 반드시 인코딩을 지정해주어야 함. 안그러면 한글 깨짐
 
-        titles = []
-        search_results = page.css('div.ss_book_box td a.bo_l b')
+        book_list = []
+        search_results = page.css('div.ss_book_box')
         search_results.each do |book|
-          titles << book.text
+          book_info = {}
+          first_column = book.css('div.ss_book_list')[0]
+          second_column = book.css('div.ss_book_list')[1]
+
+          book_info[:title] = first_column.css('a.bo_l b').text
+          book_info[:sub_title] = first_column.css('span.ss_f_g2').text
+
+          # 특별한 순위를 매기는 경우 li 태그가 하나 더 늘어남 ex) 청소년 100위 2주
+          if first_column.css('span.ss_ht2').empty?
+            author_list = first_column.css('li')[1].text.split(' | ')
+          else
+            author_list = first_column.css('li')[2].text.split(' | ')
+          end
+
+          book_info[:author] = author_list[0].strip
+          book_info[:publisher] = author_list[1].strip
+          book_info[:pub_date] = author_list[2].strip
+
+          book_info[:price] = second_column.css('span.ss_p2 b').text
+          book_info[:stock] = second_column.css('span.ss_p4 b').text
+
+          book_list << book_info
         end
-        result[store] = titles
+        result[STORE_LISTS[store]] = book_list
       end
       render json: result
     end
